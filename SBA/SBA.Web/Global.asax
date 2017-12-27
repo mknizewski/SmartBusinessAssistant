3c1c39bed712895b853ad9a528cca3f5d98774c3
Mateusz Kniżewski <mateusz.knizewski@gmail.com>
2017-12-27 13:32:25 +0100

    Zadanie #21 - TypeScript, Minification, Bundling.

---
+++ SBA/SBA.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SBA.Web.MvcApplication" Language="C#" %>
---
+++ SBA/SBA.Web/Scripts/TypeScript/main.ts
@@ -0,0 +1,25 @@
+﻿$(() => {
+    let cat = new Example("Mruczek", 6);
+
+    cat.getInfo();
+});
+
+// przykładowe użycie TypeScript 
+// do nauki - potem do wyjebki
+// pełna dokumentacja -> https://www.tutorialspoint.com/typescript/
+class Example {
+    catName: string;
+    catAge: number;
+
+    constructor(
+        catName: string,
+        catAge: number) {
+        this.catName = catName;
+        this.catAge = catAge;
+    }
+
+    getInfo(): void {
+        let fullInfo = `Imię: ${this.catName}, Wiek: ${this.catAge}`;
+        alert(fullInfo);
+    }
+}
