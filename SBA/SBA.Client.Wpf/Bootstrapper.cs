79663ceb6d79e031b1e1c5254f7c1ed28431e732
lukwesolowski <luklgmobile@gmail.com>
2018-03-21 13:59:17 +0100

    Zadanie #30 - podpięcie bibliotek + wygenerowanie projektu

---
+++ SBA/SBA.Client.Wpf/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using SBA.Client.Wpf.ViewModels;
+using System.Windows;
+
+namespace SBA.Client.Wpf
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainPanelViewModel>();
+        }
+    }
+}
