f5338baf8b71e4e091567f5e2176931bf9811d80
piotr210694 <piotr210694@wp.pl>
2018-03-22 20:47:57 +0100

    Zadanie #29 - artykuly, pozostalo o nas

---
+++ SBA/SBA.Web/Scripts/select2main.js
@@ -0,0 +1,9 @@
+﻿// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function () {
+    $('.js-example-basic-single').select2({
+        placeholder: "Sortowanie...",
+        allowClear: true,
+        theme: "classic",
+        data: [{ id: 0, text: 'Nazwa [A-Z]' }, { id: 1, text: 'Nazwa [Z-A]' }, { id: 2, text: 'Data [Od najnowszych]' }, { id: 3, text: 'Data [Od najstarszych]' }, { id: 4, text: 'Popularne' }]
+    });
+});
