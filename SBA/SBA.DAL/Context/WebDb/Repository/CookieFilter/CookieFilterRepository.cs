472f7bcd15e70f1d2dcbaad7690041f3cd8c4809
piotr210694 <piotr210694@wp.pl>
2018-04-19 13:08:16 +0200

    Zadanie #29 - drobne zmiany w cookieFilter

---
+++ SBA/SBA.DAL/Context/WebDb/Repository/CookieFilter/CookieFilterRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SBA.DAL.Context.WebDb.Repository.CookieFilter
+{
+    public interface ICookieFilterRepository : IBaseRepository
+    {
+        void SaveToLog(List<string> parametresToSave);
+    }
+
+    class CookieFilterRepository : BaseRepository, ICookieFilterRepository
+    {
+        public void SaveToLog(List<string> parametresToSave)
+        {
+            //
+        }
+    }
+}
