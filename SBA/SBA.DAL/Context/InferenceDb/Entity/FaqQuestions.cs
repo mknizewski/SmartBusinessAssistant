ada219f6a17d57f9031203a21f86990ac5689936
Mateusz Kniżewski <mateusz.knizewski@gmail.com>
2018-04-25 19:57:04 +0200

    Zadanie #47 - repo + service faq

---
+++ SBA/SBA.DAL/Context/InferenceDb/Entity/FaqQuestions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SBA.DAL.Context.InferenceDb.Entity
+{
+    public class FaqQuestions
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public DateTime InsertTime { get; set; }
+    }
+}
